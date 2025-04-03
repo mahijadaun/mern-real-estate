5c35c7838d1b7e505b0c52b9d986e0e307518eae
Mahi Jadaun <jadaunmahi17@gmail.com>
2025-04-03 21:22:07 +0530

    install react js and tailwind css and create the first template

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Make sure all JS and JSX files are included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
